39523be7de02df080d38a00db2889e3db2374300
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-07-18 10:58:24 +0800

    feat: 新增 `MultiRecognition` 自定义识别算法 (#523)

--- agent/custom/reco/__init__.py
+++ agent/custom/reco/__init__.py
@@ -1,9 +1,12 @@
+from .general import *
 from .bank import *
 from .activity import *
 from .combat import *
 
 __all__ = [
+    "MultiRecognition",
     "BankShop",
     "ActivityRe_releaseChapter",
+    "FindFirstUnplayedStageByCheckmark",
     "StagePromotionComplete",
 ]
